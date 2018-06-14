92a275f64614f498cf533b6a8e953d617ee6e5d8
rahul-soshte <rahul.soshte47@gmail.com>
2018-06-14 13:28:58 +0530

    Login and SignUp CSS Template

---
+++ createaccount2.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>EMI Pay</title>
+<link rel="stylesheet" type="text/css" href="css/createaccount.css">
+</head>
+<body>
+<div class="register-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="Name" id="name" />
+      <input type="text" placeholder="Email Address" id="email"/>
+      <input type="text" placeholder="Confirm Email Address" id="email_address_confirm"/> 
+      <input type="password" placeholder="Password" id="password"/>
+      <input type="button" id="createaccountbutton" onclick="location.href='userdashboard.php';" value="Create Account" />
+      <span class="message">Already registered? <a href="index.php">SIGN IN</a></span>
+    </form>
+  </div>
+</div>
+</body>
+</script> 
+</html>
---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>EMI Pay</title>
+<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+<div class="login-page">
+  <div class="form">
+    <form class="login-form">
+      <input type="text" placeholder="Email ID" id="email"/>
+      <input type="password" placeholder="password" id="pass"/>
+      <input type="button" id="loginbutton" onclick="location.href='userdashboard.php';" value="Login" />
+      <span class="message">Not registered? <a href="createaccount2.php">CREATE ACCOUNT</a></span>
+    </form>
+  </div>
+</div>
+</body>
+</html>
